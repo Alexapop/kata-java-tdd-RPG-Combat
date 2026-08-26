a18dcf22f48ca3581602efbf3f6b6ceb2d71b40e
Ioana <ioana3popescu@gmail.com>
2026-08-26 18:30:51 +0200

    test: add tests for the first iteration

--- src/test/java/com/factoriaf5/kata/CharacterTest.java
+++ src/test/java/com/factoriaf5/kata/CharacterTest.java
@@ -1,14 +1,58 @@
 package com.factoriaf5.kata;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
 public class CharacterTest {
-    
+
     @Test
-    public void ExampleTest() {
-        assertEquals(2, 2);
+    public void attackedCharacterShouldBeAliveIfDamageIsLowerThanHealth() {
+        Character attacker = new Character();
+        Character attacked = new Character();
+
+        attacker.characterDamagesCharacter(attacked, 300);
+
+        assertEquals(700, attacked.getHealth());
+        assertTrue(attacked.isAlive());
     }
 
+    @Test
+    public void attackedCharacterShouldBeDeadIfDamageIsHigherThanHealth() {
+        Character attacker = new Character();
+        Character attacked = new Character();
+
+        attacker.characterDamagesCharacter(attacked, 1003);
+
+        assertEquals(0, attacked.getHealth());
+        assertFalse(attacked.isAlive());
+    }
+
+    @Test
+    public void characterShouldHealIfAlive() {
+        Character healer = new Character();
+        Character healed = new Character();
+
+        healer.characterDamagesCharacter(healed, 400);
+        healer.characterHealsCharacter(healed, 200);
+
+        assertEquals(800, healed.getHealth());
+        assertTrue(healed.isAlive());
+
+    }
+
+    @Test
+    public void characterHealthShouldRemainAt1000IfHealingExceedsMaximum() {
+        Character healer = new Character();
+        Character healed = new Character();
+
+        healer.characterDamagesCharacter(healed, 100);
+        healer.characterHealsCharacter(healed, 300);
+
+        assertEquals(1000, healed.getHealth());
+        assertTrue(healed.isAlive());
+
+    }
 }
