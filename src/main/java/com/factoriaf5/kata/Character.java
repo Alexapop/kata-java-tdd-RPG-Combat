0e24983cfd006af313eeffabb31ebad6592a4cfc
Ioana <ioana3popescu@gmail.com>
2026-08-26 16:27:52 +0200

    feat: implement first iteration

--- src/main/java/com/factoriaf5/kata/Character.java
+++ src/main/java/com/factoriaf5/kata/Character.java
@@ -1,5 +1,45 @@
 package com.factoriaf5.kata;
 
 public class Character {
-    
+    private int health;
+    private int level;
+    private boolean alive;
+
+    public Character() {
+        health = 1000;
+        level = 1;
+        alive = true;
+    }
+
+    public void characterDamagesCharacter(Character attacked, int damage) {
+        attacked.health = attacked.health - damage;
+
+        if (attacked.health > 0) {
+            attacked.alive = true;
+        } else {
+            attacked.health = 0;
+            attacked.alive = false;
+        }
+
+    }
+
+    public void characterHealsCharacter(Character healed, int healing) {
+        if (!healed.alive) {
+            return;
+        }
+        healed.health = Math.min(1000, healed.health + healing);
+
+    }
+
+    public int getHealth(){
+        return health;
+    }
+
+    public int getLevel(){
+        return level;
+    }
+
+    public boolean isAlive(){
+        return alive;
+    }
 }
