8f44e886f8fd62cb2c9c39c428e81d3e7fc52512
Ioana <ioana3popescu@gmail.com>
2026-08-28 23:34:20 +0200

    feat: implement the second iteration

--- src/main/java/com/factoriaf5/kata/Character.java
+++ src/main/java/com/factoriaf5/kata/Character.java
@@ -11,35 +11,44 @@ public class Character {
         alive = true;
     }
 
-    public void characterDamagesCharacter(Character attacked, int damage) {
-        attacked.health = attacked.health - damage;
-
-        if (attacked.health > 0) {
-            attacked.alive = true;
-        } else {
-            attacked.health = 0;
-            attacked.alive = false;
-        }
-
-    }
-
-    public void characterHealsCharacter(Character healed, int healing) {
-        if (!healed.alive) {
+    public void characterDamagesCharacter(Character target, int damage) {
+        if (this == target) {
             return;
         }
-        healed.health = Math.min(1000, healed.health + healing);
+        if (target.level >= this.level + 5) {
+            damage = damage / 2;
+        } else if (this.level >= target.level + 5) {
+            damage = damage + damage / 2;
+        }
+
+        target.health = target.health - damage;
+
+        if (target.health > 0) {
+            target.alive = true;
+        } else {
+            target.health = 0;
+            target.alive = false;
+        }
 
     }
 
-    public int getHealth(){
+    public void characterHealsItself(int healing) {
+        if (!alive) {
+            return;
+        }
+        health = Math.min(1000, health + healing);
+
+    }
+
+    public int getHealth() {
         return health;
     }
 
-    public int getLevel(){
+    public int getLevel() {
         return level;
     }
 
-    public boolean isAlive(){
+    public boolean isAlive() {
         return alive;
     }
 }
